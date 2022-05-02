91682587fccb10ff83e6e4c66b01050940b13966
rocioa6 <rocioa6@gmail.com>
2022-05-02 19:47:15 +0200

    Iteration#3

--- loops.js
+++ loops.js
@@ -40,3 +40,15 @@ for (const alumn of alumns) {
   }
 }
 console.log(alumns);
+
+/* **Iteración #3: Probando For...of**
+
+Usa un bucle forof para recorrer todos los destinos del array. Imprime en un ***console.log*** sus valores.
+
+Puedes usar este array: */
+
+const placesToTravel = ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']
+for (const places of placesToTravel) {
+    console.log(places);
+    
+}
