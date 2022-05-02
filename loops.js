13e4bbfd5251ad74dca239c09ea15bba9be17578
rocioa6 <rocioa6@gmail.com>
2022-05-02 19:41:50 +0200

    Iteration#2

--- loops.js
+++ loops.js
@@ -15,3 +15,28 @@ for (const product of products) {
     console.log(product);
   }
 }
+/* **Iteración #2: Condicionales avanzados**
+
+Comprueba en cada uno de los usuarios que tenga al menos dos trimestres aprobados y añade la propiedad ***isApproved*** a true o false en consecuencia. Una vez lo tengas compruébalo con un ***console.log***.
+ */
+
+const alumns = [
+  { name: "Pepe Viruela", T1: false, T2: false, T3: true },
+  { name: "Lucia Aranda", T1: true, T2: false, T3: true },
+  { name: "Juan Miranda", T1: false, T2: true, T3: true },
+  { name: "Alfredo Blanco", T1: false, T2: false, T3: false },
+  { name: "Raquel Benito", T1: true, T2: true, T3: true },
+];
+
+for (const alumn of alumns) {
+  if (alumn["T1"] && alumn["T2"]) {
+    alumn["isAproved"] = true;
+  } else if (alumn["T2"] && alumn["T3"]) {
+    alumn["isAproved"] = true;
+  } else if (alumn["T1"] && alumn["T3"]) {
+    alumn["isAproved"] = true;
+  } else {
+    alumn["isAproved"] = false;
+  }
+}
+console.log(alumns);
